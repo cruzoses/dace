0d4c101917ea4607ced6fce4bb347afccec85a8c
cruzoses <cruzoses@users.noreply.github.com>
2026-08-04 22:30:52 -0400

    chore: activar timestamp de assets en produccion para forzar recarga de CSS/JS

--- config/app.php
+++ config/app.php
@@ -90,6 +90,7 @@ return [
      * enable timestamping regardless of debug value.
      */
     'Asset' => [
+        'timestamp' => 'force',
         //'timestamp' => true,
         // 'cacheTime' => '+1 year'
     ],
